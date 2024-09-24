f35b57f1fa34393b1e7ad53a3108164f34f0348a
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-09-24 18:11:20 -0500

    Implemented the API AuthenticationFilter

--- src/main/java/com/authservice/apigateway/service/AuthenticationFilter.java
+++ src/main/java/com/authservice/apigateway/service/AuthenticationFilter.java
@@ -2,17 +2,23 @@ package com.authservice.apigateway.service;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Component
 public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
 
     private final RouteValidator validator;
+    private final JwtUtils jwtUtils;
 
-    public AuthenticationFilter(RouteValidator validator) {
+    public AuthenticationFilter(RouteValidator validator, JwtUtils jwtUtils) {
         super(Config.class);
         this.validator = validator;
+        this.jwtUtils = jwtUtils;
     }
 
     @Override
@@ -21,10 +27,40 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
             var request = exchange.getRequest();
 
             ServerHttpRequest serverHttpRequest = null;
-            if(validator.isSecured.test(request)) {
+            if(validator.isSecured.test((ServerHttpRequest) request)) {
+                if(authMissing((ServerHttpRequest) request)){
+                    return onError(exchange, HttpStatus.UNAUTHORIZED);
+                }
 
+                String authHeader = exchange.getRequest().getHeaders().get("Authorization").get(0);
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                } else {
+                    return onError(exchange, HttpStatus.UNAUTHORIZED);
+                }
+
+                if(jwtUtils.tokenIsExpired(authHeader)) {
+                    return onError(exchange, HttpStatus.UNAUTHORIZED);
+                }
+
+                serverHttpRequest = (ServerHttpRequest) exchange.getRequest()
+                        .mutate()
+                        .header("userIdRequest", jwtUtils.extractUserId(authHeader).toString())
+                        .build();
             }
+            return chain.filter(exchange.mutate().request((org.springframework.http.server.reactive.ServerHttpRequest)
+                    serverHttpRequest).build());
         });
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, HttpStatus httpStatus) {
+        ServerHttpResponse response = (ServerHttpResponse) exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        return null;
+    }
+
+    private boolean authMissing(ServerHttpRequest request) {
+        return !request.getHeaders().containsKey("Authorization");
     }
 
     public static class Config {}
--- src/main/java/com/authservice/apigateway/service/JwtUtils.java
+++ src/main/java/com/authservice/apigateway/service/JwtUtils.java
@@ -1,4 +1,37 @@
 package com.authservice.apigateway.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
 public class JwtUtils {
+
+    private final String secretKey = "djhfjklaadfjalkdfhlKFALKJFJLAKFHDLKJfhlkjdfhalkjfhalkjdfhdalkjfhalskjfhadsjlkfahsljkfdh";
+
+    public Claims getClaims(String token) {
+        return (Claims) Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenIsExpired(String token) {
+        try {
+            return getClaims(token).getExpiration().before(new Date());
+        } catch (Exception e) {
+            return true;
+        }
+    }
+
+    public Integer extractUserId(String token) {
+        try {
+            return Integer.parseInt(getClaims(token).getSubject());
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
